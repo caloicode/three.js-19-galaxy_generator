da3e949bb741f27929591e96567862656bf8125b
unknown <carolinecodilla@gmail.com>
2025-11-14 13:14:56 +0800

    build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,20 @@ import restart from "vite-plugin-restart";
 
 export default defineConfig({
   root: "src/", // Source files live here (includes index.html)
+  base: "/three.js-19-galaxy_generator/",
   publicDir: "../static/", // Static assets to be copied as-is during build
+
   server: {
     host: true, // Accessible via local network
     open: true, // Automatically opens browser on start
   },
+
   build: {
     outDir: "../dist", // Build output directory
     emptyOutDir: true, // Clears /dist before new build
     sourcemap: true, // Generates sourcemaps for debugging
   },
+  
   plugins: [
     restart({
       restart: ["../static/**"], // Restart dev server when static assets change
